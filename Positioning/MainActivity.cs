2d975bcf688058743ce564f7f21a4693e5e447f4
Hanks <Hanks@HanksdeMacBook.local>
2017-01-21 00:26:23 +0800

    追加裝置GPS功能是否啟用判斷

--- Positioning/MainActivity.cs
+++ Positioning/MainActivity.cs
@@ -29,35 +29,35 @@ namespace Positioning
 			tvLatitude = FindViewById<TextView>(Resource.Id.tvLatitude);
 			tvLongtitude = FindViewById<TextView>(Resource.Id.tvLongitude);
 			tvProvider = FindViewById<TextView>(Resource.Id.tvProvider);
-			//if (!locationManager.IsProviderEnabled(LocationManager.GpsProvider)){
-			//	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			//	builder.SetTitle("定位管理")
-			//		   .SetMessage("GPS尚未啟動\n是否啟用GPS..?")
-			//	       .SetPositiveButton("啟用", handler)
-			//		   .SetNegativeButton("不啟用", null)
-			//		   .Create()
-			//		   .Show();
-			//}
+			if (!locationManager.IsProviderEnabled(LocationManager.GpsProvider)){
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.SetTitle("定位管理")
+					   .SetMessage("GPS尚未啟動\n是否啟用GPS..?")
+				       .SetPositiveButton("啟用", handler)
+					   .SetNegativeButton("不啟用", null)
+					   .Create()
+					   .Show();
+			}
 
-			//handler = new EventHandler<DialogClickEventArgs>((object sender, DialogClickEventArgs e) => 
-			//{
-			//	Intent intent = new Intent(
-			//});
-			button.Click += delegate {
-				button.Text = "Location Service Running..";
-				locationProvider = LocationManager.GpsProvider;
-				if (locationManager.IsProviderEnabled(locationProvider))
-				{
-					// 每一分鐘(6000毫秒)或每一公尺更新一次位置訊息
-					//locationManager.RequestLocationUpdates(locationProvider, 6000, 1, this);
-					//即時更新
-					locationManager.RequestLocationUpdates(locationProvider, 0, 0, this);
-				} else {
-					Toast.MakeText(this, locationProvider + " available. Does the device have location services enable ?", ToastLength.Short).Show();
-					locationProvider = LocationManager.NetworkProvider;
-					locationManager.RequestLocationUpdates(locationProvider, 0, 0, this);
-				}
-			};
+			handler = new EventHandler<DialogClickEventArgs>((object sender, DialogClickEventArgs e) => 
+			{
+				Intent intent = new Intent(
+			});
+			//button.Click += delegate {
+			//	button.Text = "Location Service Running..";
+			//	locationProvider = LocationManager.GpsProvider;
+			//	if (locationManager.IsProviderEnabled(locationProvider))
+			//	{
+			//		// 每一分鐘(6000毫秒)或每一公尺更新一次位置訊息
+			//		//locationManager.RequestLocationUpdates(locationProvider, 6000, 1, this);
+			//		//即時更新
+			//		locationManager.RequestLocationUpdates(locationProvider, 0, 0, this);
+			//	} else {
+			//		Toast.MakeText(this, locationProvider + " available. Does the device have location services enable ?", ToastLength.Short).Show();
+			//		locationProvider = LocationManager.NetworkProvider;
+			//		locationManager.RequestLocationUpdates(locationProvider, 0, 0, this);
+			//	}
+			//};
 		}
 
 		protected override void OnResume()
